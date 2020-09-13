05c403ba7f3ae13207095478c326a5abff015c5a
Zakk Lundin <zakk.lundin@skola.botkyrka.se>
2020-09-13 20:33:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let str = prompt("Type_Password")
+
+if (str.length >= 8){
+    alert('för långt')
+}
+
+if (str.length <= 6){
+    alert('Too short m8')
+}
+
+if (str.length = 7){
+    alert('Korrekt')
+}
